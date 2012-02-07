f3d83d855641549919a67c272166caccc1800c78
Andrej Karpathy <andrej.karpathy@gmail.com>
2012-02-07 01:07:38 -0800

    Adding keyboard events, random numbers, vector class, other small changes. Also adding a simple starter game.

--- npgmain.js
+++ npgmain.js
@@ -1,8 +1,13 @@
+//Simple game engine
+//Author: Andrej Karpathy
+//License: BSD
 //This function does all the boring canvas stuff. To use it, just create functions:
 //update()          gets called every frame
 //draw()            gets called every frame
 //myinit()          gets called once in beginning
 //mouseClick(x, y)  gets called on mouse click
+//keyUp(keycode)    gets called when key is released
+//keyDown(keycode)  gets called when key is pushed
 
 var canvas;
 var ctx;
@@ -36,14 +41,36 @@ function drawRect(x, y, w, h){
     ctx.rect(x,y,w,h);
     ctx.closePath();
     ctx.fill();
+    ctx.stroke();
 }
 
 function drawCircle(x, y, r){
     ctx.beginPath();
     ctx.arc(x, y, r, 0, Math.PI*2, true); 
     ctx.closePath();
+    ctx.stroke();
     ctx.fill();
 }
+
+//uniform distribution
+function randf(s, e) {
+    return Math.random()*(e-s) + s;
+}
+
+//normal distribution random number
+function randn(mean, variance) {
+  var V1, V2, S;
+  do {
+    var U1 = Math.random();
+    var U2 = Math.random();
+    V1 = 2 * U1 - 1;
+    V2 = 2 * U2 - 1;
+    S = V1 * V1 + V2 * V2;
+  } while (S > 1);
+  X = Math.sqrt(-2 * Math.log(S) / S) * V1;
+  X = mean + Math.sqrt(variance) * X;
+  return X;
+  }
 
 function eventClick(e) {
     
@@ -64,15 +91,34 @@ function eventClick(e) {
     mouseClick(x, y);
 }
 
-function init(){
+//event codes can be found here:
+//http://www.aspdotnetfaq.com/Faq/What-is-the-list-of-KeyCodes-for-JavaScript-KeyDown-KeyPress-and-KeyUp-events.aspx
+function eventKeyUp(e) {
+    var keycode = ('which' in e) ? e.which : e.keyCode;
+    keyUp(keycode);
+}
+
+function eventKeyDown(e) {
+    var keycode = ('which' in e) ? e.which : e.keyCode;
+    keyDown(keycode);
+}
+
+function init(FPS){
+    //takes frames per secont to run at
+    
     canvas = document.getElementById('mycanvas');
     ctx = canvas.getContext('2d');
     WIDTH = canvas.width;
     HEIGHT = canvas.height;
     canvas.addEventListener('click', eventClick, false);
     
-    FPS = 10; //frames per secont to run at
-    setInterval(tick, FPS);
+    //canvas element cannot get focus by default. Requires to either set 
+    //tabindex to 1 so that it's focusable, or we need to attach listeners
+    //to the document. Here we do the latter
+    document.addEventListener('keyup', eventKeyUp, true);
+    document.addEventListener('keydown', eventKeyDown, true);
+    
+    setInterval(tick, 1000/FPS);
     
     myinit();
 }
